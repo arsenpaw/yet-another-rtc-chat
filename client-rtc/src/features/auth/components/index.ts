3e0e73875f1e59c8873118304396a9f43c9b3051
aquammarine <mrguysy@gmail.com>
2026-05-28 20:28:58 +0300

    feat: add and update barrel files

--- client-rtc/src/features/auth/components/index.ts
+++ client-rtc/src/features/auth/components/index.ts
@@ -1,2 +1,4 @@
 export { LoginButton } from "./LoginButton.tsx";
 export { RegisterButton } from "./RegisterButton.tsx";
+export { LogoutButton } from "./LogoutButton.tsx";
+export { ProtectedRoute } from "./ProtectedRoute.tsx";
