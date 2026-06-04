b376b7bbb627ef8c6887dfb8dcd20024ab5fe442
aquammarine <mrguysy@gmail.com>
2026-06-04 17:44:13 +0300

    feat: setup tanstack query

--- client-rtc/src/app/main.tsx
+++ client-rtc/src/app/main.tsx
@@ -6,24 +6,28 @@ import { AuthProvider } from "./providers/AuthProvider.tsx";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider
-      >
-        <App />
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          pauseOnHover
-          theme="dark"
-        />
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider
+        >
+          <App />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            pauseOnHover
+            theme="dark"
+          />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 );
