7de84fd821446151deab8f2fbfa153f87aaa1060
aquammarine <mrguysy@gmail.com>
2026-06-12 00:59:25 +0300

    style: change baseUrl for api and add server urls to config

--- client-rtc/src/shared/api/clientApi.ts
+++ client-rtc/src/shared/api/clientApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { config } from '../config';
 
 export const api = axios.create({
-    baseURL: config.apiBaseUrl,
+    baseURL: `${config.apiBaseUrl}/api`,
 });
 
 api.interceptors.request.use(config => {
--- client-rtc/src/shared/config/config.ts
+++ client-rtc/src/shared/config/config.ts
@@ -1,8 +1,17 @@
 export const config = {
-    apiBaseUrl: import.meta.env.VITE_API_BASE_URL as string || 'http://localhost:5000/api',
+    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
     signalingHubPath: '/hubs/signaling',
-
     get signalingHubUrl(): string {
         return `${this.apiBaseUrl}${this.signalingHubPath}`;
     },
+    rtcConfig: {
+        iceServers: [
+            {
+                urls: [
+                    'stun:stun1.l.google.com:19302',
+                    'stun:stun2.l.google.com:19302',
+                ],
+            },
+        ],
+    } as RTCConfiguration,
 } as const;
